af79691f0144cb83a5e8459cf86136dec6f0d90f
Eric Chu <echu113@gmail.com>
2013-11-07 00:17:00 -0800

    State generation should work now. See in code comments.

--- project1.hs
+++ project1.hs
@@ -125,40 +125,86 @@ reverseBoard board = map reverse board
 columnizeBoard :: [String] -> [String]
 columnizeBoard board = board
 
--- ERIC'S CODE STARTS HERE
-
-generateStr1 :: String -> String -> Int -> String
-generateStr1 str1 str2 index	= replaceListElem str1 index '-'
-	
-generateStr2s :: String -> String -> Int -> [String]
-generateStr2s str1 str2 index	= generateStr2s' str1 str2 index
-	
-generateStr2s' :: String -> String -> Int -> [String]
-generateStr2s' str1 str2 index
-	| length str1 > length str2		= generateStr2sLongShort (str1 !! index) str2 index
-	| length str1 < length str2		= generateStr2sShortLong (str1 !! index) str2 index
-	| otherwise						= [] 
-	
-generateStr2sLongShort :: Char -> String -> Int -> [String]
-generateStr2sLongShort pawn str2 index
-	| index == 0			= [makeMove str2 0 pawn]
-	| index == length str2	= [makeMove str2 (index - 1) pawn]
-	| otherwise				= (makeMove str2 (index) pawn) : [(makeMove str2 (index - 1) pawn)]
-
-generateStr2sShortLong :: Char -> String -> Int -> [String]
-generateStr2sShortLong pawn str2 index
-	= (makeMove str2 (index) pawn) : [(makeMove str2 (index + 1) pawn)]
-	
-makeMove :: String -> Int -> Char -> String
-makeMove str2 index pawn
-	| canMove pawn (str2 !! index)	= replaceListElem str2 index pawn
-	| otherwise						= []
 
 
--- utils
+-- ERIC'S CODE STARTS HERE **** state generation working!
+-- will add more comments
+-- currently, calling generateNewStates given a board and player ('w' or 'b'), all states one step away from the current
+-- state will be returned as a list. Score evaluation is current set to 0.
+-- also, implementation assumes that 'b' pawns move start at the bottom and move upward while 'w' pawns start at the top
+-- and move downward. 
+
+generateNewStates :: [String] -> Char -> [([String], Int)]
+generateNewStates currBoard player	
+	| player == 'w'		= generateNewStates' currBoard player (getAllPawns currBoard player)
+	| player == 'b'		= reverseNewStates (generateNewStates' (reverse currBoard) player (getAllPawns (reverse currBoard) player))
+	| otherwise			= []
+
+reverseNewStates :: [([String], Int)] -> [([String], Int)]
+reverseNewStates states
+	| null states 	= []
+	| otherwise 	= (reverse (fst (head states)), snd (head states)) : reverseNewStates (tail states)
+
+generateNewStates' :: [String] -> Char -> [(Int, Int)] -> [([String], Int)]
+generateNewStates' currBoard player pawnlocs
+	| null pawnlocs		= []
+	| otherwise			= generateNewStatesPawn currBoard player (head pawnlocs) ++ generateNewStates' currBoard player (tail pawnlocs)
+
+generateNewStatesPawn :: [String] -> Char -> (Int, Int) -> [([String], Int)]
+generateNewStatesPawn currBoard player pawnloc 	
+	= filter (\x -> not (null (fst x))) ((generateNewStatePawnMvLeft currBoard player pawnloc) : (generateNewStatePawnMvRight currBoard player pawnloc) : 
+											(generateNewStatePawnJumpLeft currBoard player pawnloc) : (generateNewStatePawnJumpRight currBoard player pawnloc) : [])
+
+generateNewStatePawnMvLeftDefault :: [String] -> Char -> (Int, Int) -> Bool -> [String]
+generateNewStatePawnMvLeftDefault currBoard player pawnloc isJump
+	| ((fst pawnloc) >= (length currBoard) - 1) || null (fst newStr2)		= []
+	| not isJump	= replaceListElem (replaceListElem currBoard (fst pawnloc) (generateStr1 (currBoard !! (fst pawnloc)) (snd pawnloc))) (fst pawnloc + 1) (fst newStr2)
+	| otherwise 	= generateNewStatePawnMvLeftDefault (replaceListElem (replaceListElem currBoard (fst pawnloc) (generateStr1 (currBoard !! (fst pawnloc)) (snd pawnloc))) (fst pawnloc + 1) (fst newStr2))
+						player ((fst pawnloc + 1), snd newStr2) False
+	where newStr2 = generateStr2MvLeft (currBoard !! (fst pawnloc)) (currBoard !! (fst pawnloc + 1)) (snd pawnloc) isJump
+
+generateNewStatePawnMvLeft :: [String] -> Char -> (Int, Int) -> ([String], Int)
+generateNewStatePawnMvLeft currBoard player pawnloc	
+	= ((generateNewStatePawnMvLeftDefault currBoard player pawnloc False), 0)
+
+generateNewStatePawnMvRight :: [String] -> Char -> (Int, Int) -> ([String], Int)
+generateNewStatePawnMvRight currBoard player pawnloc	
+	= (reverseBoard (generateNewStatePawnMvLeftDefault (reverseBoard currBoard) player (fst pawnloc, (length (currBoard !! (fst pawnloc)) - (snd pawnloc + 1))) False), 0)
+
+generateNewStatePawnJumpLeft :: [String] -> Char -> (Int, Int) -> ([String], Int)
+generateNewStatePawnJumpLeft currBoard player pawnloc
+	= ((generateNewStatePawnMvLeftDefault currBoard player pawnloc True), 0)
+
+generateNewStatePawnJumpRight :: [String] -> Char -> (Int, Int) -> ([String], Int)
+generateNewStatePawnJumpRight currBoard player pawnloc
+	= (reverseBoard (generateNewStatePawnMvLeftDefault (reverseBoard currBoard) player (fst pawnloc, (length (currBoard !! (fst pawnloc)) - (snd pawnloc + 1))) True), 0)
+
+generateStr1 :: String -> Int -> String
+generateStr1 str1 index	= replaceListElem str1 index '-'
+	
+generateStr2MvLeft :: String -> String -> Int -> Bool -> (String, Int)
+generateStr2MvLeft str1 str2 index isJump
+	| ((length str1 - length str2) == 1) && (index > 0)					= makeMove str2 (index - 1) (str1 !! index) isJump
+	| ((length str1 - length str2) == -1) && (index < length str1)		= makeMove str2 index (str1 !! index) isJump
+	| otherwise															= ([], 0)
+	
+makeMove :: String -> Int -> Char -> Bool -> (String, Int)
+makeMove str2 index pawn isJump
+	| (isJump && canJump pawn (str2 !! index)) || (not isJump && canMove pawn (str2 !! index))	
+					= ((replaceListElem str2 index pawn), index)
+	| otherwise						= ([], 0)
+
+
+-- helper functions
 	
 canMove :: Char -> Char -> Bool
 canMove pawn1 pawn2		= (not (isEmpty pawn1)) && isEmpty pawn2
+
+canJump :: Char -> Char -> Bool
+canJump pawn1 pawn2
+	| (pawn1 == 'w') && (pawn2 == 'b')	= True
+	| (pawn1 == 'b') && (pawn2 == 'w') 	= True
+	| otherwise							= False
 
 isEmpty :: Char -> Bool
 isEmpty block =		block == '-'
@@ -170,8 +216,25 @@ replaceListElem list index elem
 	| otherwise		= (head list) : replaceListElem (tail list) (index - 1) elem		
 	
 getAllPawns :: [String] -> Char -> [(Int, Int)]
-getAllPawns board player
-	| null board	= []
-	| otherwise		= getAllPawnsRow (head board) player : getAllPawns (tail) player
+getAllPawns board player	= getAllPawns' board player 0
+
+getAllPawns' :: [String] -> Char -> Int -> [(Int, Int)]
+getAllPawns' board player rowNum
+	| null board 	= []
+	| otherwise 	= getAllPawnsRow (head board) player rowNum ++ getAllPawns' (tail board) player (rowNum + 1)
 	
---getAllPawns :: String -> Char -> (Int, Int)
+getAllPawnsRow :: String -> Char -> Int -> [(Int, Int)]
+getAllPawnsRow row player rowNum	= getAllPawnsRow' row player rowNum 0
+
+getAllPawnsRow' :: String -> Char -> Int -> Int -> [(Int, Int)]
+getAllPawnsRow' row player rowNum colNum
+	| null row 				= []
+	| (head row) == player	= (rowNum, colNum) : getAllPawnsRow' (tail row) player rowNum (colNum + 1)
+	| otherwise				= getAllPawnsRow' (tail row) player rowNum (colNum + 1)
+
+reverseBoard :: [String] -> [String]
+reverseBoard board = map reverse board
+
+
+
+
